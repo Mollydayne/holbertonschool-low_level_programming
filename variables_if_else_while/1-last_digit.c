2d7c7a3dec16fe7b8508d0608c283cf450700c59
Mollydayne <cl.w.perez@gmail.com>
2024-10-22 10:05:28 +0200

    last digit v11

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
 }
 else if (lastD == 0)
 {
-printf("Last digit of %d is 0 and is 0\n", lastD);
+printf("Last digit of %d is 0 and is 0\n", n);
 }
 else
 {
