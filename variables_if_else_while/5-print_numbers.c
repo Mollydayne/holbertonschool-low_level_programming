8644d8c2fdccf51acf6666eb224950b6666406c0
Mollydayne <cl.w.perez@gmail.com>
2024-10-22 11:25:50 +0200

    numbers v1

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include  <stdio.h>
+
+/**
+*main - Entry point of the program
+*
+*Description: Prints numbers of base 10
+*Return: always 0
+*/
+
+int main(void)
+{
+printf("0123456789\n");
+return (0);
+}
