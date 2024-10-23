45f5cbfbc2ee4729edafcf6fa219e5ea1c90ba18
Mollydayne <cl.w.perez@gmail.com>
2024-10-23 14:21:02 +0200

    Jack Bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+*jack_bauer- prints every minute 
+* @n : number
+*Return: n or -n
+*/
+void jack_bauer(void)
+int = heures, minutes; 
+for (heures = 0; heures <= 23; heures ++)
+{
+for (minutes = 0; minutes <= 59; minutes ++)  
+_putchar((heures / 10) + '0');
+_putchar((heures % 10) + '0');
+_putchar(':');
+_putchar((minutes / 10) + '0');
+_putchar((minutes % 10) + '0');
+}
