d843ceabc4c9270edb6d89c7bca82516aa2be5e7
Mollydayne <cl.w.perez@gmail.com>
2024-10-23 15:12:39 +0200

    times table

---
+++ functions_nested_loops/9-times_tables.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+*jack_bauer- prints every minute
+*
+*Return: n or -n
+*/
+void times_table(void);
+int chiffre, multiplicateur, resultat
+
+for (chiffre = 0; i <= 9; i++)
+{
+_putchar('0');
+}
+for (multiplicateur = 0; i <=9; i++)
+{
+_putchar(multiplicateur)
+_putchar(' ');
+_putchar(',');
+}
+for (resultat = 0; i <= 9; i++)
+_putchar(resultat);
+}
+_putchar('\n');
+}
