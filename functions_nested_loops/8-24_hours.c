a220aea2f41e36cea89792f6368d7664f937ee96
Mollydayne <cl.w.perez@gmail.com>
2024-10-23 14:31:10 +0200

    jack bauer v3

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,7 +6,8 @@
 *Return: n or -n
 */
 void jack_bauer(void)
-int = heures, minutes;
+{
+int heures, minutes;
 for (heures = 0; heures <= 23; heures++)
 {
 for (minutes = 0; minutes <= 59; minutes++)
@@ -16,5 +17,7 @@ _putchar((heures % 10) + '0');
 _putchar(':');
 _putchar((minutes / 10) + '0');
 _putchar((minutes % 10) + '0');
+_putchar('\n');
+}
 }
 }
