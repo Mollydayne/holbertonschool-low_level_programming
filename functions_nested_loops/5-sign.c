a70c2606840a058a08f2293460588430093f2e83
Mollydayne <cl.w.perez@gmail.com>
2024-10-23 11:28:58 +0200

    Sign V3

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
 *print_sign - prints the sign of a number
 * @n : character that need to be checked
-*Return: 0 or 1
+*Return: 0 or or -1
 */
 int print_sign(int n)
 {
@@ -11,13 +11,14 @@ if (n > 0)
 _putchar ('+');
 return (1);
 }
+else if (n == 0)
 {
-else if (n == 0);
 _putchar ('0');
 return (0);
 }
-{
 else
+{
 _putchar ('-');
 return (-1);
+}
 }
