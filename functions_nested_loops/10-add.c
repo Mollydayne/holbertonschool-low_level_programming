419129ccec309b9519c7e249cbfd7e0a73e5a24d
Mollydayne <cl.w.perez@gmail.com>
2024-10-24 14:12:05 +0200

    a+b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+*int add- addition de deux chiffres
+*
+*Return: resultat
+*/
+int add(int, int);
+int a, b, resultat;
+{
+resultat = a + b;
+return (resultat);
+}
