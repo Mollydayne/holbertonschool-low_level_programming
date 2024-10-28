b1215d04c4bb27897a2921e9340bb847684599c5
Mollydayne <cl.w.perez@gmail.com>
2024-10-28 09:53:45 +0100

    Isupper V1

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*print_to_98- print from n to 98
+*
+*Return: n
+*/
+void print_to_98(int n)
+{
+  int n = -10;
+  for (n = -10 ; n < 98 ; n++)
+    {
+      if (n != 98)
+	printf(", %d", n);
+      return('n');
+    }
+}
