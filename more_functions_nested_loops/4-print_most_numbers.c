6addfda5dabe42e17b8bb0bfcee619f0b9215ee2
Mollydayne <cl.w.perez@gmail.com>
2024-10-28 10:35:50 +0100

    Most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+ *print_most_numbers - print numbers from 0 to 9 byt not 2 and 4
+*
+*Return: void (no return value)
+*/
+void print_most_numbers(void)
+{
+char ch = '0';
+for (ch = '0'; ch <= '9'; ch++)
+{
+if (ch == '2' || ch == '4')
+{
+continue;
+}
+_putchar(ch);
+_putchar('\n');
+}
