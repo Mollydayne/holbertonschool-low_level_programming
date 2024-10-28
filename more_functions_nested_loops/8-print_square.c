c09cef0b12b24bc85ace5fe0c8a84cd5af4b0743
Mollydayne <cl.w.perez@gmail.com>
2024-10-28 18:06:36 +0100

    Print square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,17 +8,16 @@
 void print_square(int size)
 {
 int a, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
 for (a = 0; a <= size; a++)
 {
-_putchar('#');
-}
 for (b = 0; b <= size; b++)
 {
 _putchar('#');
-_putchar('\n');
 }
-{
-if (size <= 0)
 _putchar('\n');
 }
 }
