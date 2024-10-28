b95acc393df919b463f80b990a993cb0c19cb451
Mollydayne <cl.w.perez@gmail.com>
2024-10-28 10:54:30 +0100

    More numbers V1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+#include "main.h"
+/**
+*more_numbers - print numbers from 0 to 14, 10 times
+*
+*Return: void (no return value)
+*/
+void more_numbers(void)
+{
+char ch = '0';
+for (ch = '0'; ch <= '14'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
