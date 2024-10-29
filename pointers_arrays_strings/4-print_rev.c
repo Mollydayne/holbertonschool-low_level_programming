e8ef247120140a8208a73d0b57989fe8dd821aa2
Mollydayne <cl.w.perez@gmail.com>
2024-10-29 10:44:51 +0100

    Reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_rev - reverse a string
+*@s : pointer to the string to reverse
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+int i;
+for (i = 0; str[i] != '\0'; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
---
+++ pointers_arrays_strings/4-print_rev.c~
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _puts - prints a string
+*@str : pointer to the string to print
+* Return: void
+*/
+
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
