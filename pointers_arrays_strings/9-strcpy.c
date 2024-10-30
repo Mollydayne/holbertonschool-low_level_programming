7c310c6ea21a79371eed0c7f604de85fe495770b
Mollydayne <cl.w.perez@gmail.com>
2024-10-30 10:12:52 +0100

    Strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_array - prints array
-*@n : number of elements of an array
-*@a : integer filling the array
-* Return: void
+* _strcpy - copy a string
+*@dest : destination array
+*@src:source string
+* Return: pointer to dest
 */
 
 char *_strcpy(char *dest, char *src)
 {
 int i = 0;
-for (i = 0; src[1]!='\0'; i++)
+for (i = 0; src[i] != '\0'; i++)
 {
-src[i] = dest[i];
-{
-printf(", ");
+dest[i] = src[i];
 }
 dest[i] = '\0';
 }
