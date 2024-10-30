f27eac515773756d0942fc211da9a8af3b274a0a
Mollydayne <cl.w.perez@gmail.com>
2024-10-30 10:34:36 +0100

    Atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _atoi - convert a string to an integer
+*@s : character
+*
+* Return: integer
+*/
+
+int _atoi(char *s)
+{
+int i;
+int num = 0;
+for(i = 0; *s[i] != 0; i++)
+num = num * 10 + *s[i] - '0';
+return num;
+}
