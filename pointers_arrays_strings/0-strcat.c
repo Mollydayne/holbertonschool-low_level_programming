110d642228e7847b2c6fdbb014804e42f9f549cc
Mollydayne <cl.w.perez@gmail.com>
2024-10-31 11:21:59 +0100

    Concaténation

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,6 +8,7 @@
 */
 char *_strcat(char *dest, char *src)
 {
+char *start = dest; //conserve l'adresse de départ de dest
 while (*dest)
 {
 ++dest;
@@ -17,5 +18,5 @@ while (*src)
 *dest++ = *src++;
 }
 *dest = '\0';
-return dest
+return start;
 }
