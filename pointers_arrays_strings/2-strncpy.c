08e275ad88217bf5e87f1adb6eff243ce002835b
Mollydayne <cl.w.perez@gmail.com>
2024-10-31 15:32:51 +0100

    Strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,16 +5,21 @@
 * _strncpy - copy strings
 *@dest : string 1
 *@src : string 2
-*@n : number of caracters to add
+*@n : number of caracters to copy
 * Return: dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-for (i = 0; src[i] != '\0'; i++)
+while (src[i] != '\0' && i < n)
 {
 dest[i] = src[i];
+i++;
 }
+while (i < n)
+{
 dest[i] = '\0';
-return dest;
+i++;
+}
+return (dest);
 }
