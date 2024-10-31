4691365259a2381f219595f8ba25a2e87059a227
Mollydayne <cl.w.perez@gmail.com>
2024-10-31 11:11:01 +0100

    Concatenation

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcat - concatenate two strings
+* 
+* Return: void (no return value)
+*/
+char *_strcat(char *dest, char *src)
+while (*dest)
+{
+++dest;
+}
+while (*src)
+{
+*dest++ = *src++;
+}
+*dest = '\0';
+}
