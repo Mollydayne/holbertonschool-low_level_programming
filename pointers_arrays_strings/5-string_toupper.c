9bdca293627d9b9ed75246a5a00c59dbb382cb4c
Mollydayne <cl.w.perez@gmail.com>
2024-11-01 19:18:29 +0100

    String toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* *string_toupper - to change lowercase to uppercase
+*@* : char to be changed
+*
+* Return: char
+*/
+char *string_toupper(char *)
+{
+char *original = s;
+while (*s != '\0')
+{
+if (*s >= 'a' && *s <= 'z')
+{
+*s = *s - 32;
+}
+s++;
+}
+return (original);
+}
