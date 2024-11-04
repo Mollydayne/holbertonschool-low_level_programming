089be922d05372acbe7f57f687028c6d5d506da1
Mollydayne <cl.w.perez@gmail.com>
2024-11-04 19:13:32 +0100

    Print Chess

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* *_strstr - locates a substring
+*@haystack : principal string
+*@needle : substring we want to locate
+*
+* Return: substring
+*/
+char *_strstr(char *haystack, char *needle)
+{
+if (*needle != '\0');
+return (haystack);
+}
+while (*haystack)
+{
+if (*haystack == *needle)
+{
+char *h = haystack;
+char *n = needle;
+while (*h && *n &&*h == *n)
+{
+h++;
+n++;
+}
+if (*n == '\0')
+{
+return (haystask);
+}
+}
+haystack++;
+}
+return (NULL);
+}
+
