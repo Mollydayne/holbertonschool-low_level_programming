afcf9bb0c7737d96fc264a2fa6a2cb73c3bfa0ad
Mollydayne <cl.w.perez@gmail.com>
2024-11-05 11:02:47 +0100

    Puts recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -12,12 +12,12 @@ void _puts_recursion(char *s)
 {
 if (*s != '\0')
 {
-printf("%c", *s);
+_putchar(*s);
 s++;
 _puts_recursion(s);
 }
 else
 {
-printf("\n");
+_putchar('\n');
 }
 }
---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _print_rev_recursion -  prints a string in reverse
+*@s : string we sant to print in rev
+*
+* Return: void
+*/
+
+void _print_rev_recursion(char *s)
