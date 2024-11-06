2144fe30aef3e7cffb80a627815cf1c1b6689b92
Mollydayne <cl.w.perez@gmail.com>
2024-11-06 14:43:59 +0100

    Correction factorial

--- recursion/main.c
+++ recursion/main.c
@@ -10,21 +10,13 @@ int main(void)
 {
     int r;
 
-    r = is_prime_number(1);
+    r = factorial(1);
     printf("%d\n", r);
-    r = is_prime_number(1024);
+    r = factorial(5);
     printf("%d\n", r);
-    r = is_prime_number(16);
+    r = factorial(10);
     printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
+    r = factorial(-1024);
     printf("%d\n", r);
     return (0);
 }
