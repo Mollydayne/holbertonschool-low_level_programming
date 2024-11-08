d5231d5b700ad39fd1c6a5c386d9cabe36eb4965
Mollydayne <cl.w.perez@gmail.com>
2024-11-08 15:23:24 +0100

    Diagonal

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,23 +2,21 @@
 #include "main.h"
 
 /**
-* print_diagsums :  prints a diagonal
-*@a : square matice to print diagonal from
-*@size : size of the matrice
-*
-* Return: void
-*/
+ * print_diagsums - prints the  diagonals
+ * of a square matrix of integers
+ * @a: square matrix of which we print the sum of diagonals
+ * @size: size of the matrix
+ */
 void print_diagsums(int *a, int size)
 {
-int *main_diag = a;
-int *secondary_diag = a + size - 1;
-int sum = 0, sum1 = 0;
-for (int i = 0; i < size; i++)
+int i;
+unsigned int sum, sum1;
+sum = 0;
+sum1 = 0;
+for (i = 0; i < size; i++)
 {
-sum += *main_diag;
-sum1 += *secondary_diag;
-main_diag += size + 1;
-secondary_diag += size - 1;
+sum += a[(size * i) + i];
+sum1 += a[(size * (i + 1)) - (i + 1)];
 }
 printf("%d, %d\n", sum, sum1);
 }
