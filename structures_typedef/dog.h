b7f0790aa3a629619afe0bd4a6fdf66d48ffd382
Mollydayne <cl.w.perez@gmail.com>
2024-11-13 20:41:51 +0100

    Dog header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - DOGGOS
+ * @name: name of a cute doggo
+ * @age: age of doggo
+ * @owner: owner of a fantastic doggo
+ * 
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+
+#endif
