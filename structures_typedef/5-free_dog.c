98b06993a9f434be77cd9f6b5beff2b2a9b26d5a
Mollydayne <cl.w.perez@gmail.com>
2024-11-14 14:15:53 +0100

    Free_dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * free_dog- frees the doggos
+ *
+ * @name: Name of the dog
+ * @d: dogs we want to free
+ *
+ * Return: pointer to new dog_t, or NULL if it fails
+ */
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+free(d->owner);
+free(d->name);
+free(d);
+}
