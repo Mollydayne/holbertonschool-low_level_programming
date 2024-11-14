b06458c35290c535f9a3a35f0df3f65e940957fa
Mollydayne <cl.w.perez@gmail.com>
2024-11-14 10:08:21 +0100

    Init dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - DOGGOS
@@ -14,6 +14,11 @@ struct dog
 char *name;
 float age;
 char *owner;
-} dog;
+};
+
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
