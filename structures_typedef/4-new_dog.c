88e3bcd29be11b74c93f477d50e669c986ac4084
Mollydayne <cl.w.perez@gmail.com>
2024-11-15 17:15:56 +0100

    New dog correction

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,42 @@
 #include <string.h>
 #include "dog.h"
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
+{
+int len = 0;
+while (*str++)
+len++;
+return (len);
+}
+
+/**
+ * _strcopy - Copies a string
+ *
+ * @dest: buffer to store string copy
+ * @src: source string
+ *
+ * Return: The pointer to dest.
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+for (index = 0; src[index]; index++)
+dest[index] = src[index];
+dest[index] = '\0';
+return (dest);
+}
+
 /**
  * new_dog - Creates a new dog
  *
