5a083db42ca7ba3981634e531d79492958127186
Mollydayne <cl.w.perez@gmail.com>
2024-11-15 14:14:31 +0100

    new dog correction

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,40 +4,35 @@
 #include "dog.h"
 
 /**
- * new_dog- function that creates a new dog
+ * new_dog - Creates a new dog
  *
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
  *
- * Return: pointer to new dog_t, or NULL if it fails
+ * Return: Pointer to new dog_t, or NULL if it fails
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog = malloc(sizeof(dog_t));
+dog_t *new_dog;
+if (name == NULL || owner == NULL)
+return (NULL);
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
-
-new_dog->name = malloc(strlen(name) + 1);
+new_dog->name = strdup(name);
 if (new_dog->name == NULL)
-
 {
 free(new_dog);
 return (NULL);
 }
-strcpy(new_dog->name, name);
-
-new_dog->owner = malloc(strlen(owner) + 1);
+new_dog->owner = strdup(owner);
 if (new_dog->owner == NULL)
-
 {
 free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
-strcpy(new_dog->owner, owner);
-
 new_dog->age = age;
 return (new_dog);
 }
