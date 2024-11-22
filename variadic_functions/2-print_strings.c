f1cb81c2f18eeac8d205c18248e4a660481506f5
Mollydayne <cl.w.perez@gmail.com>
2024-11-22 09:55:18 +0100

    Print strings correction

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,10 +13,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 va_list args;
 unsigned int i = 0;
 char *str;
-if (n == 0)
-return;
+
 va_start(args, n);
 for (i = 0; i < n; i++)
+{
 str = va_arg(args, char *);
 if (str == NULL)
 printf("NILL");
@@ -24,6 +24,7 @@ else
 printf("%s", str);
 if (separator != NULL && i < n - 1)
 printf("%s", separator);
+}
 printf("\n");
 va_end(args);
 }
