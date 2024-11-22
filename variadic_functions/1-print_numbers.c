821bf441f3902730fccaebd86bb5b023f785d522
Mollydayne <cl.w.perez@gmail.com>
2024-11-22 09:46:32 +0100

    Print number correction

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,10 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i = 0;
 va_list args;
 
-if (n == 0)
-printf("\n");
-return;
-
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
