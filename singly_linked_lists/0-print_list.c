3b06384d8691a8277085af010ceb60559fe9c0ac
Mollydayne <cl.w.perez@gmail.com>
2024-12-02 15:37:10 +0100

    Betty correction for task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,3 +1,14 @@
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the head of the list_t list.
+ *
+ * Description: This function traverses a singly linked list of type list_t,
+ *              printing each element's string and its length. If an element's
+ *              string is NULL, it prints "[0] nil". The function returns the
+ *              total number of nodes in the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 #include <stddef.h>
 #include <stdio.h>
 
@@ -5,16 +16,16 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] nil\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        node_count++;
-        h = h->next;
-    }
-    return (node_count);
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] nil\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+node_count++;
+h = h->next;
+}
+return (node_count);
 }
