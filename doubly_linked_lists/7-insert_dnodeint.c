a4ad7ad24b9697eb2c8f7ab47e6602acef01eef1
Mollydayne <cl.w.perez@gmail.com>
2024-12-03 11:48:31 +0100

    Rename previous directory and adding .gitignore to every project to clean the repo

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,5 +14,36 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    
+	dlistint_t *new_node, *temp = *h;
+	unsigned int i = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (temp != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (temp == NULL && i < idx - 1)
+		return (NULL);
+
+	if (temp != NULL && temp->next == NULL && i == idx - 1)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = temp->next;
+	new_node->prev = temp;
+
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+
+	temp->next = new_node;
+
+	return (new_node);
 }
