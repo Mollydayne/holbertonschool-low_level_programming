41f68589fb488c48dbacff07a8d0e1238d0d8c3d
Mollydayne <cl.w.perez@gmail.com>
2024-12-06 08:31:41 +0100

    task 2- append_test_to_file for project file I/O

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,12 +17,37 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    
+	int fd;
+	int bytes_written;
+	int text_length = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		if (close(fd) == -1)
+			return (-1);
+		return (1);
+	}
+
+	   while (text_content[text_length] != '\0')
+		   text_length++;
+
+	   bytes_written = write(fd, text_content, text_length);
+	   if (bytes_written == -1 || bytes_written != text_length)
+	   {
+		   close(fd);
+		   return (-1);
+	}
+
+
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
 }
